3a406b3afad1dc0531e46efe0c95c1b08eea097b
Jan Mercl <jan.mercl@nic.cz>
2012-03-09 13:43:17 +0100

    Add the mersenne package
    The first naive/most simple implementation of FromFactor. Estimated
    performance is in O(n^2) or worse.
    
    $ go test -bench B
    PASS
    BenchmarkNew_1e1	 5000000	       412 ns/op
    BenchmarkNew_1e2	 5000000	       439 ns/op
    BenchmarkNew_1e3	 5000000	       516 ns/op
    BenchmarkNew_1e4	 1000000	      1137 ns/op
    BenchmarkNew_1e5	  500000	      6508 ns/op
    BenchmarkNew_1e6	   50000	     61942 ns/op
    BenchmarkNew_1e7	    5000	    582636 ns/op
    BenchmarkNew_1e8	     200	   9011665 ns/op
    BenchmarkHasFactorUint32Rnd	 5000000	       691 ns/op
    --- BENCH: BenchmarkHasFactorUint32Rnd
    	all_test.go:318: Random 32 bit factor, random 32 bit exponent
    
    BenchmarkHasFactorUint64Rnd	  100000	     27123 ns/op
    --- BENCH: BenchmarkHasFactorUint64Rnd
    	all_test.go:344: Random 64 bit factor, random 32 bit exponent
    
    BenchmarkHasFactorBigIntRnd_128b	   50000	     73816 ns/op
    --- BENCH: BenchmarkHasFactorBigIntRnd_128b
    	all_test.go:375: Random 128 bit factor, random 32 bit exponent
    
    BenchmarkHasFactorBigInt_104b	   50000	     35097 ns/op
    --- BENCH: BenchmarkHasFactorBigInt_104b
    	all_test.go:399: Verify a 104 bit factor of M100279 (16.6 bit exponent)
    
    BenchmarkHasFactorBigIntMod104b	   10000	    164248 ns/op
    --- BENCH: BenchmarkHasFactorBigIntMod104b
    	all_test.go:415: Verify a 104 bit factor of M100279 (16.6 bit exponent) using big.Int.Mod
    
    BenchmarkHasFactorBigInt_137b	   50000	     50993 ns/op
    --- BENCH: BenchmarkHasFactorBigInt_137b
    	all_test.go:432: Verify a 137 bit factor of M7293457 (22.8 bit exponent)
    
    BenchmarkHasFactorBigIntMod137b	     100	  11167520 ns/op
    --- BENCH: BenchmarkHasFactorBigIntMod137b
    	all_test.go:448: Verify a 137 bit factor of M7293457 (22.8 bit exponent) using big.Int.Mod
    
    BenchmarkFromFactorBigInt20b_1e1	 1000000	       1357 ns/op
    BenchmarkFromFactorBigInt20b_1e2	  500000	       7602 ns/op
    BenchmarkFromFactorBigInt20b_1e3	   20000	      82311 ns/op
    BenchmarkFromFactorBigInt20b_1e4	    1000	    2159702 ns/op
    BenchmarkFromFactorBigInt20b_1e5	      10	  148960200 ns/op
    BenchmarkFromFactorBigInt20b_1e6	       1	14602832000 ns/op
    BenchmarkFromFactorBigInt137b_1e1	 1000000	       1663 ns/op
    BenchmarkFromFactorBigInt137b_1e2	  500000	       7661 ns/op
    BenchmarkFromFactorBigInt137b_1e3	   20000	      88107 ns/op
    BenchmarkFromFactorBigInt137b_1e4	    1000	    2208817 ns/op
    BenchmarkFromFactorBigInt137b_1e5	      10	  151161500 ns/op
    BenchmarkFromFactorBigInt137b_1e6	       1	14677768000 ns/op
    ok  	github.com/cznic/mathutil/mersenne	98.369s
    $

---
+++ mersenne/mersenne.go
@@ -0,0 +1,107 @@
+// Copyright (c) 2011 jnml. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package mersenne collects utilities related to Mersenne numbers[1] and/or some
+of their properties.
+
+Exponent
+
+In this documentatoin the term 'exponent' refers to 'n' of a Mersenne number Mn
+equal to 2^n-1. This package supports only uint32 sized exponents. New supports
+exponents only up to math.MaxInt32.
+
+Links
+
+Referenced from above:
+ [1] http://en.wikipedia.org/wiki/Mersenne_number
+*/
+package mersenne
+
+import (
+	"github.com/cznic/mathutil"
+	"math"
+	"math/big"
+)
+
+var (
+	_0 = big.NewInt(0)
+	_1 = big.NewInt(1)
+	_2 = big.NewInt(2)
+)
+
+// New returns Mn == 2^n-1 for n <= math.MaxInt32 or nil otherwise.
+func New(n uint32) (m *big.Int) {
+	if n > math.MaxInt32 {
+		return
+	}
+
+	m = big.NewInt(0)
+	return m.Sub(m.SetBit(m, int(n), 1), _1)
+}
+
+// HasFactorUint32 returns true if d | Mn. Typical run time for a 32 bit factor
+// and a 32 bit exponent is < 1 µs.
+func HasFactorUint32(d, n uint32) bool {
+	return d == 1 || d&1 != 0 && mathutil.ModPowUint32(2, n, d) == 1
+}
+
+// HasFactorUint64 returns true if d | Mn. Typical run time for a 64 bit factor
+// and a 32 bit exponent is < 30 µs.
+func HasFactorUint64(d uint64, n uint32) bool {
+	return d == 1 || d&1 != 0 && mathutil.ModPowUint64(2, uint64(n), d) == 1
+}
+
+// HasFactorBigInt returns true if d | Mn, d > 0. Typical run time for a 128
+// bit factor and a 32 bit exponent is < 75 µs.
+func HasFactorBigInt(d *big.Int, n uint32) bool {
+	return d.Cmp(_1) == 0 || d.Sign() > 0 && d.Bit(0) == 1 &&
+		mathutil.ModPowBigInt(_2, big.NewInt(int64(n)), d).Cmp(_1) == 0
+}
+
+/*
+FromFactorBigInt returns n such that d | Mn if n <= max and d is odd. In other
+cases zero is returned.
+
+Every odd d ∊ N divides infinitely many Mersenne numbers. The returned n is the
+exponent of smallest such Mn.
+
+NOTE: The computation of n from a given d performs in O(n). It is thus highly
+recomended to use the 'max' argument to limit the "searched" exponent upper
+bound as appropriate. Otherwise the computation can take a long time as a large
+factor can be a divisor of a Mn with exponent above the uint32 limits.
+
+Implementation is restricted to exponents <= math.MaxInt32.
+
+The FromFactorBigInt function is a Go implementation of the original Will
+Edgington's "reverse method", discussed here:
+http://tech.groups.yahoo.com/group/primenumbers/message/15061
+*/
+func FromFactorBigInt(d *big.Int, max uint32) (n uint32) {
+	if d.Bit(0) == 0 {
+		return
+	}
+
+	var m, kd big.Int
+	m.Set(d)
+	kd.Set(d)
+	var i, last int
+	for {
+		if m.Bit(i) == 0 {
+			if i == m.BitLen() {
+				return uint32(i)
+			}
+
+			kd.Lsh(&kd, uint(i-last))
+			last = i
+			m.Add(&m, &kd)
+		}
+		if uint32(i) == max || i == math.MaxInt32 {
+			return 0
+		}
+
+		i++
+	}
+	panic("unreachable")
+}
