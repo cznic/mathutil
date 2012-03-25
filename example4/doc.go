7e1f25c6638574fe988b03334ec650a0195191d5
Jan Mercl <0xjnml@gmail.com>
2012-03-25 11:51:12 +0200

    Add PrimorialProducts example

---
+++ example4/doc.go
@@ -0,0 +1,13 @@
+/*
+
+Let QRN be the number of quadratic residues of N.  Let Q be QRN/N.  From a
+sorted list of primorial products < 2^32 find "record breakers".  "Record
+breaker" is N with new lowest Q.
+
+There are only 49 "record breakers" < 2^32.
+
+To run the example
+ $ go run main.go
+
+*/
+package documentation
